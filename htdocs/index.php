591e29703d0922c552f80277061efd22fe3075e6
Kevin Warrington <kevin.warrington@gmail.com>
2014-03-11 18:37:59 -0600

    reorganize post hash data

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,44 +7,30 @@ $client = new Predis\Client();
 # clear db
 $client->flushdb();
 
-// Articles
-$client->hset('post:1', 'author', 'admin');
-$client->hset('post:1', 'title', 'Article 1');
-$client->hset('post:1', 'content', 'Lorem Ipsum Dolor Sit Amet');
-$client->hset('post:1', 'date', 20090304);
-$client->zadd('post:date', 20090304, 1); // add date to sorted set
+// add articles
+$client->hset('posts', 1, json_encode(array('id' => 1, 'author' => 'admin', 'title' => 'Article 1', 'content' => 'Lorem Ipsum Dolor Sit Amet')));
+$client->hset('posts', 2, json_encode(array('id' => 2, 'author' => 'admin', 'title' => 'Article 2', 'content' => 'Lorem Ipsum Dolor Sit Amet')));
+$client->hset('posts', 3, json_encode(array('id' => 3, 'author' => 'admin', 'title' => 'Article 3', 'content' => 'Lorem Ipsum Dolor Sit Amet')));
+$client->hset('posts', 4, json_encode(array('id' => 4, 'author' => 'admin', 'title' => 'Article 4', 'content' => 'Lorem Ipsum Dolor Sit Amet')));
 
-$client->hset('post:2', 'author', 'admin');
-$client->hset('post:2', 'title', 'Article 2');
-$client->hset('post:2', 'content', 'Lorem Ipsum Dolor Sit Amet');
-$client->hset('post:2', 'date', 20100304);
+// add dates
+$client->zadd('post:date', 20090304, 1);
 $client->zadd('post:date', 20100304, 2);
-
-$client->hset('post:3', 'author', 'admin');
-$client->hset('post:3', 'title', 'Article 3');
-$client->hset('post:3', 'content', 'Lorem Ipsum Dolor Sit Amet');
-$client->hset('post:3', 'date', 20110304);
 $client->zadd('post:date', 20110304, 3);
-
-$client->hset('post:4', 'author', 'admin');
-$client->hset('post:4', 'title', 'Article 4');
-$client->hset('post:4', 'content', 'Lorem Ipsum Dolor Sit Amet');
-$client->hset('post:4', 'date', 20120304);
 $client->zadd('post:date', 20120304, 4);
 
 // find posts within range
 $postIds = $client->zrangebyscore('post:date', 20100101, 20111231);
 
-// retrieve posts
-$pipeline = $client->pipeline();
-foreach ($postIds as $postId) {
-	$pipeline->hgetall("post:{$postId}");
-}
-$posts = $pipeline->execute();
+// retrieve posts by id
+$posts = call_user_func_array(
+	array($client, "hmget"),
+	array_merge(array('posts'), $postIds)
+);
 
 // render
 foreach ($posts as $post) {
-	$post = (object) $post;
+	$post = json_decode($post);
 	echo "<h1>{$post->title}</h1>";
 	echo "<p>{$post->content}</p>";
 }
